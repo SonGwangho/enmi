99e21e7584d48f6273c59a449181aaa5f804e86b
SonGwangho <SonGwangho>
2023-03-13 08:12:28 +0900

    new file:   .gitignore 	new file:   build.gradle 	new file:   gradle/wrapper/gradle-wrapper.jar 	new file:   gradle/wrapper/gradle-wrapper.properties 	new file:   gradlew 	new file:   gradlew.bat 	new file:   settings.gradle 	new file:   src/main/java/com/enmi/emWeb/EmWebApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/com/enmi/emWeb/EmWebApplicationTests.java

---
+++ src/main/java/com/enmi/emWeb/EmWebApplication.java
@@ -0,0 +1,13 @@
+package com.enmi.emWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmWebApplication.class, args);
+	}
+
+}
